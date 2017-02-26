d76ec446eef00c80e3f59cf7f0cccb714f18c4fe
sksbrg <stefan@kesberg.de>
2017-02-26 10:50:02 +0100

    Lets see, what happened? We finally have a working Aurelia application. To get there we removed our starter 'index.html' file, introduced the first Aurelia packages (more than intended but during the initial bundling process it became clear that additional Aurelia libraries are needed to fulfill all dependencies), and added our first Aurelia view showcasing two-way binding already.

---
+++ src/Accountant.Web/src/main.js
@@ -0,0 +1,10 @@
+﻿export function configure(aurelia) {
+    aurelia.use
+        .basicConfiguration()
+        .developmentLogging();
+
+    aurelia.start().then(a => { 
+        //this loads our app.js in the body element.
+        a.setRoot('app/app', document.body);
+    });
+}
--- src/Accountant.Web/webpack.config.js
+++ src/Accountant.Web/webpack.config.js
@@ -1,11 +1,31 @@
 ﻿// https://github.com/ampedandwired/html-webpack-plugin
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// https://github.com/aurelia/webpack-plugin
+var AureliaWebPackPlugin = require('aurelia-webpack-plugin');
+
+var webpack = require('webpack');
+var path = require('path');
+
+const project = require('./package.json');
+
+
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        // the app's entry point, this should be set automatically by 'aurelia-webpack-plugin'
+        // using the files in the 'src' folder
+        // the app knows which of the files is our entry point because of the 'aurelia-bootstrapper-webpack'
+        // who reads the entry point from the aurelia-app="main" in our index.html
+        // app: [],
+        // TODO: does not work as intended; needed to reference 'main.js' manually
+        app: ['./src/main.js'],
+        // all Aurelia libraries will be saved into a dedicated bundle
+        // to ease maintenance we read the required packages from the 'dependencies' section of our 'package.json' file
+        aurelia: Object.keys(project.dependencies).filter(dep => dep.startsWith('aurelia-'))
+    },
     output: {
         path: './wwwroot',
-        filename: 'scripts/bundle.js'
+        filename: 'scripts/[name].bundle.js'
     },
     module: {
         rules: [{
@@ -18,9 +38,18 @@ module.exports = {
                     presets: ['es2015']
                 }
             }]
+        },
+        {
+            // this loader reads the referenced html templates and bundles them with the javascript files, e.g. 'app.html'
+            test: /\.html$/,
+            loader: 'html-loader',
+            exclude: path.resolve('src/index.html')
         }]
     },
     plugins: [
+        new AureliaWebPackPlugin(),
+        // creates the seperate bundle for Aurelia libraries
+        new webpack.optimize.CommonsChunkPlugin({ name: ['aurelia'] }),
         new HtmlWebpackPlugin({
             template: 'index.html'
         })
